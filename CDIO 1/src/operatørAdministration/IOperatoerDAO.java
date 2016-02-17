a9dac1de528a61fa0f91211966786ef070ca13a5
Sebastian <sebastian@vestrup.net>
2016-02-17 11:31:44 +0100

    Starten på CDIO 1 - Interfaces

---
+++ CDIO 1/src/operatørAdministration/IOperatoerDAO.java
@@ -0,0 +1,10 @@
+package operatørAdministration;
+
+public class IOperatoerDAO 
+{
+	OperatoerDTO getOperatoer(int oprId) throws DALException;
+	List<OperatoerDTO> getOperatoerList() throws DALException;
+	void createOperatoer(OperatoerDTO opr) throws DALException;
+	void updateOperatoer(OperatoerDTO opr) throws DALException;
+	void deleteOperatoer(OperatoerDTO opr) throws DALException;
+}
---
+++ CDIO 1/src/operatørAdministration/OperatoerDTO.java
@@ -0,0 +1,13 @@
+package operatørAdministration;
+
+public class OperatoerDTO 
+{
+	int 	oprId;                     
+	String 	oprNavn;                
+	String 	ini;                 
+	int    	cpr;                 
+	String	password;
+	// I skal selv færdiggøre klasseerklæringen og -definitionen med setter- og getter metoder.
+	
+	
+}
