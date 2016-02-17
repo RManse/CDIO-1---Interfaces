ac1e3add61e519ade48880bb545f5c15128f96bf
Sebastian <sebastian@vestrup.net>
2016-02-17 13:42:42 +0100

    Ubetydelig rettelse.

--- CDIO 1/src/operatørAdministration/OperatoerDAO.java
+++ CDIO 1/src/operatørAdministration/OperatoerDAO.java
@@ -1,8 +1,6 @@
 package operatørAdministration;
 import java.util.*;
 
-import bmi.Data;
-import bmi.IData;
 import exception.DALException;
 import operatørAdministration.Data.OperatoerDTO;
 
@@ -14,31 +12,31 @@ public class OperatoerDAO implements IOperatoerDAO {
 	
 	@Override
 	public OperatoerDTO getOperatoer(int oprId) throws DALException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public List<OperatoerDTO> getOperatoerList() throws DALException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void createOperatoer(OperatoerDTO opr) throws DALException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void updateOperatoer(OperatoerDTO opr) throws DALException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void deleteOperatoer(OperatoerDTO opr) throws DALException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
