ed0de13f79148d3372da26860fd417b1a5b97e31
Magnus <Magnus_enevoldsen@live.dk>
2016-02-19 13:27:56 +0100

    test for at se om jeg kan committe igen

---
+++ CDIO 1/src/operatørAdministration/ToDO.java
@@ -0,0 +1,11 @@
+package operatørAdministration;
+
+public class ToDO {
+	
+}
+
+//Nemmere todo liste
+
+//Du skal kunne logge ud som operatør
+//Det skal være muligt at logge ind som en anden operatør end nr 10 efter den er oprettet
+//...
